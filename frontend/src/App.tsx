dda6525e3b9e649c1af09e245706b5e3759d743e
Gerard Hager <ghager93@gmail.com>
2025-07-22 17:50:31 +1000

    Changes to ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,57 +4,49 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 
 const GMT_TIMEZONE = 'UTC'
 
+const API_URL = 'http://localhost:8000/api'
+const READING_LIMIT = 100
+
+
 function App() {
-  const [remoteReadings, setRemoteReadings] = useState<Array<{value: number, timestamp: number}>>([])
+  const [readings, setReadings] = useState<Array<{value: number, timestamp: number}>>([])
+  const [from, setFrom] = useState<number>(Math.floor(Date.now()/1000-86400))
+  const [to, setTo] = useState<number>(Math.floor(Date.now()/1000))
+  const [granularity, setGranularity] = useState<string>('1m')
+  const [isLive, setIsLive] = useState<boolean>(true)
+  const [currTime, setCurrTime] = useState<number>(Date.now()/1000)
 
-  const handleFetchRemote = async () => {
+  const handleFetchReadings = async () => {
     try {
-      const res = await fetch('http://localhost:8000/api/glucose-readings/remote')
-      if (!res.ok) {
-        const errBody = await res.text()
-        console.error(`Remote fetch failed with status ${res.status}:`, errBody)
-        throw new Error(`Network response was not ok: ${res.status}`)
-      }
-    } catch (err) {
-      console.error('Failed to fetch remote readings:', err)
-      alert('Error fetching remote readings')
-    }
-  }
-
-  // Fetch readings from DB via backend API
-  const handleFetchValues = async (from?: number) => {
-    try {
+      console.log('Fetching readings')
       let res
-      if (from) {
-        res = await fetch(`http://localhost:8000/api/glucose-readings?limit=100&order=desc&granularity=1m&from=${from}`)
+      if (isLive) {
+        res = await fetch(`${API_URL}/glucose-readings?order=desc&granularity=${granularity}&from=${from}`)
       } else {
-        res = await fetch('http://localhost:8000/api/glucose-readings?limit=100&order=desc&granularity=1m')
+        res = await fetch(`${API_URL}/glucose-readings?order=desc&granularity=${granularity}&from=${from}&to=${to}`)
       }
-      console.log('Response:', res)
       if (!res.ok) {
         const errBody = await res.text()
         console.error(`Remote fetch failed with status ${res.status}:`, errBody)
         throw new Error(`Network response was not ok: ${res.status}`)
       }
       const dataRaw: Array<{value: number, timestamp: number | string}> = await res.json()
-      // Convert ISO timestamp strings to numeric epoch seconds
       const data = dataRaw.map(r => ({
         value: r.value,
         timestamp: typeof r.timestamp === 'string'
           ? Math.floor(new Date(r.timestamp).getTime() / 1000)
           : r.timestamp
       }))
-      console.log('Data:', data)
-      setRemoteReadings(data)
+      setReadings(data)
     } catch (err) {
-      console.error('Failed to fetch remote readings:', err)
-      alert('Error fetching remote readings')
+      console.error('Failed to fetch readings:', err)
+      alert('Error fetching readings')
     }
   }
 
   // Automatically load readings on component mount
   useEffect(() => {
-    handleFetchValues()
+    handleFetchReadings()
   }, [])
 
   // Real-time stream data
@@ -63,14 +55,14 @@ function App() {
     const eventSource = new EventSource('http://localhost:8000/api/glucose-readings/stream')
     eventSource.onmessage = (event) => {
       console.log('Received SSE:', event.data)
-      console.log('length of readings:', remoteReadings.length)
+      // console.log('length of readings:', remoteReadings.length)
       // const newEntry: string = event.data
       // setTestData(prev => [...prev, newEntry])
       try {
         // Have to parse twice to get the correct format
         const parsed: Array<{value: number, timestamp: number}> = JSON.parse(JSON.parse(event.data))
         console.log('Parsed:', parsed)
-        setRemoteReadings(prev => [...parsed, ...prev])
+        // setRemoteReadings(prev => [...parsed, ...prev])
       } catch (err) {
         console.error('Failed to parse SSE data:', err)
       }
@@ -82,15 +74,44 @@ function App() {
     return () => {
       eventSource.close()
     }
-  }, [remoteReadings])
+  }, [])
 
   // Call for new readings every minute
   useEffect(() => {
-    const interval = setInterval(() => {
-      handleFetchValues(from=Date.now()/1000)
+    const interval = setInterval(async () => {
+        try {
+          console.log('Fetching new readings')
+          const lastReadingTime = readings[0].timestamp
+          const res = await fetch(`${API_URL}/glucose-readings?order=desc&granularity=1m&from=${lastReadingTime}`)
+          if (!res.ok) {
+            const errBody = await res.text()
+            console.error(`Remote fetch failed with status ${res.status}:`, errBody)
+            throw new Error(`Network response was not ok: ${res.status}`)
+          }
+          const dataRaw: Array<{value: number, timestamp: number | string}> = await res.json()
+          const data = dataRaw.map(r => ({
+            value: r.value,
+            timestamp: typeof r.timestamp === 'string'
+              ? Math.floor(new Date(r.timestamp).getTime() / 1000)
+              : r.timestamp
+          }))
+          const dataNew = data.filter(r => r.timestamp > lastReadingTime)
+          setReadings(prev => [...dataNew, ...prev])
+        } catch (err) {
+          console.error('Failed to fetch new readings:', err)
+          alert('Error fetching new readings')
+        }
     }, 60000)
     return () => clearInterval(interval)
+  }, [readings])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrTime(Math.floor(Date.now()/1000))
+    }, 1000)
+    return () => clearInterval(interval)
   }, [])
+
 
   return (
     <div className="container">
@@ -99,13 +120,8 @@ function App() {
         <p>Track and manage your glucose readings</p>
       </header>
 
-      {/* Button to load remote readings */}
-      <div style={{ marginBottom: '20px' }}>
-        <button onClick={handleFetchRemote}>Load Remote Readings</button>
-      </div>
-
       {/* Table of remote readings */}
-      {remoteReadings.length > 0 && (
+      {readings.length > 0 && (
         <div className="readings-section">
           <h2>Remote Readings</h2>
           <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
@@ -117,7 +133,7 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {remoteReadings.map((r, idx) => (
+                {readings.map((r, idx) => (
                   <tr key={idx}>
                     <td>{r.value}</td>
                     <td>{new Date(r.timestamp * 1000)
@@ -129,11 +145,23 @@ function App() {
             </table>
           </div>
 
+          {/* Most recent reading */}
+          <h2>Most Recent Reading</h2>
+          <p>{readings[0].value} mmoL</p>
+          <p>{new Date(readings[0].timestamp * 1000)
+            .toLocaleString('en-AU', { timeZone: GMT_TIMEZONE })
+          }</p>
+
+          {/* Time since last reading */}
+          <h2>Time Since Last Reading</h2>
+          <p>{currTime - readings[0].timestamp + 600 * 60} seconds</p>
+          <p>{currTime} {readings[0].timestamp - 600 * 60}</p>
+
           {/* Line chart */}
           <h2>Glucose Trend</h2>
           <ResponsiveContainer width="100%" height={300}>
             <LineChart
-              data={[...remoteReadings].reverse()}
+              data={[...readings].reverse()}
               margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
